2add84bdb0dc3d24d90ecf226d47e326abc30ff0
Olha Abrosimova <abrosimova2020@ukr.net>
2022-08-29 19:12:17 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ export default function App() {
 	return (
 		<div className="App">
 			<h1>Weather app Abrosimova</h1>
-			This app was built by{" "}
-			<a
-				href="https://olhaabrosimova.netlify.app/about.html"
-				target="_blank"
-				rel="noreferrer"
-			>
-				Olha Abrosimova
-			</a>{" "}
-			and it is open-sourced on{" "}
-			<a
-				href="https://github.com/OlhaAbrosimova/abr-react-weather-2908"
-				target="_blank"
-				rel="noreferrer"
-			>
-				{" "}
-				GitHub
-			</a>
+			<footer>
+				This app was built by{" "}
+				<a
+					href="https://olhaabrosimova.netlify.app/about.html"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Olha Abrosimova
+				</a>{" "}
+				and it is open-sourced on{" "}
+				<a
+					href="https://github.com/OlhaAbrosimova/abr-react-weather-2908"
+					target="_blank"
+					rel="noreferrer"
+				>
+					{" "}
+					GitHub
+				</a>
+			</footer>
 		</div>
 	);
 }
