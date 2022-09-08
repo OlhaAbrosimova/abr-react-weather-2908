e152d7000bc975b4e510898f4bef2422e3ca265d
Olha Abrosimova <o.abrosimova2012@gmail.com>
2022-09-08 13:32:35 +0300

    snother city serch forecast

--- src/Weather.js
+++ src/Weather.js
@@ -8,6 +8,8 @@ export default function Weather(props) {
 	const [weatherData, setWeatherData] = useState({ ready: false });
 	const [city, setCity] = useState(props.defaultCity);
 
+	
+
 	function handleResponse(response) {
 		// console.log(response.data);
 		setWeatherData({
